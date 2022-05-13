d1c27b1c2f931804a1aa60b9f1b2a7a121047416
Pablo Valentin <95161515+PabloValentin94@users.noreply.github.com>
2022-05-12 21:17:04 -0300

    Update Metal_Lords.xaml.cs

--- App_XamarinFlix/App_XamarinFlix/Filmes/Comedia/Metal_Lords.xaml.cs
+++ App_XamarinFlix/App_XamarinFlix/Filmes/Comedia/Metal_Lords.xaml.cs
@@ -23,10 +23,10 @@ namespace App_XamarinFlix.Filmes.Comedia
 
             /*var htmlSource = new HtmlWebViewSource();
 
-            htmlSource.Html = @"<iframe width="560" height="315" 
-                                 src="https://www.youtube.com/embed/BZicnf4BckI" 
-                                 title="YouTube video player" frameborder="0" allow="accelerometer; 
-                                 autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+            htmlSource.Html = @"<iframe width='560' height='315' 
+                                 src='https://www.youtube.com/embed/BZicnf4BckI' 
+                                 title='YouTube video player' frameborder='0' allow='accelerometer; 
+                                 autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture' 
                                  allowfullscreen></iframe>";
 
            Visualizador.Source = htmlSource;*/
