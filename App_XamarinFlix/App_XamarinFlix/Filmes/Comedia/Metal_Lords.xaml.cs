3d089aa32d6c4f66bb8269746a602ade00ff8872
PABLO VALENTIN <pablo.valentin@etec.sp.gov.br>
2022-05-18 13:45:43 -0300

    Alterado.

--- App_XamarinFlix/App_XamarinFlix/Filmes/Comedia/Metal_Lords.xaml.cs
+++ App_XamarinFlix/App_XamarinFlix/Filmes/Comedia/Metal_Lords.xaml.cs
@@ -24,7 +24,7 @@ namespace App_XamarinFlix.Filmes.Comedia
             /*var htmlSource = new HtmlWebViewSource();
 
             htmlSource.Html = @"<iframe width='560' height='315' 
-                                 src='https://www.youtube.com/embed/BZicnf4BckI' 
+                                 src='https://www.youtube.com/embed/GU0FscrlFow?start=6' 
                                  title='YouTube video player' frameborder='0' allow='accelerometer; 
                                  autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture' 
                                  allowfullscreen></iframe>";
