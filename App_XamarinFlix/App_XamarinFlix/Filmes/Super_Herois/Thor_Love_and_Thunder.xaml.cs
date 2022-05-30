3fa067c31ab4dbc97fb12c04deee0028656da152
Pablo Valentin <95161515+PabloValentin94@users.noreply.github.com>
2022-05-30 19:22:32 -0300

    Update Thor_Love_and_Thunder.xaml.cs
    Trailer Trocado.

--- App_XamarinFlix/App_XamarinFlix/Filmes/Super_Herois/Thor_Love_and_Thunder.xaml.cs
+++ App_XamarinFlix/App_XamarinFlix/Filmes/Super_Herois/Thor_Love_and_Thunder.xaml.cs
@@ -23,10 +23,10 @@ namespace App_XamarinFlix.Filmes.Super_Herois
 
             /*var htmlSource = new HtmlWebViewSource();
 
-            htmlSource.Html = @"<iframe width='560' height='315' 
-                                 src='https://www.youtube.com/embed/dPcdCZekMQg' 
-                                 title='YouTube video player' frameborder='0' allow='accelerometer; 
-                                 autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture' 
+            htmlSource.Html = @"<iframe width="560" height="315" 
+                                 src="https://www.youtube.com/embed/1c_W_4cNLn0" 
+                                 title="YouTube video player" frameborder="0" allow="accelerometer; 
+                                 autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
                                  allowfullscreen></iframe>";
 
             Visualizador.Source = htmlSource;*/
