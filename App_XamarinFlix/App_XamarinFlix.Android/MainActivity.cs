6a43951954c93ee90d6be4d137dd359523380924
PABLO VALENTIN <pablo.valentin@etec.sp.gov.br>
2022-06-14 19:57:22 -0300

    Mudando a cor da barra superior da tela.

--- App_XamarinFlix/App_XamarinFlix.Android/MainActivity.cs
+++ App_XamarinFlix/App_XamarinFlix.Android/MainActivity.cs
@@ -17,6 +17,11 @@ namespace App_XamarinFlix.Droid
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
             LoadApplication(new App());
+
+            Window.ClearFlags(Android.Views.WindowManagerFlags.TranslucentStatus);
+            Window.AddFlags(Android.Views.WindowManagerFlags.DrawsSystemBarBackgrounds);
+            Window.SetStatusBarColor(Android.Graphics.Color.ParseColor("#2f2f30"));
+
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
